dbde419571c19ba13ca03877571936c8c56282db
yutian.liu <939029835@qq.com>
2021-02-19 16:47:47 +0800

    修复使用haxelib命令运行时无法正常编译直接指定路径的问题。

--- tools/RunBuild.hx
+++ tools/RunBuild.hx
@@ -1,10 +1,14 @@
 package;
 
+import sys.FileSystem;
+
 class RunBuild {
 	static function main() {
 		trace("梦工厂编译开始");
 		trace(Sys.args(), Sys.programPath());
 		var path = Sys.args()[0] + "Export/mgc";
+		if(!FileSystem.exists(path))
+			path = Sys.args()[0];
 		Sys.command("python3 " + StringTools.replace(Sys.programPath(), "run.n", "mgc-build.py") + " " + path + " zygameui apk");
 	}
 }
